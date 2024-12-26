6f3bd3bc227c0d129022ad03d7fa28fe48e2c98d
Mostafa Zeynali <121800106+mzeynali-ir@users.noreply.github.com>
2024-12-26 14:53:48 +0330

    first commit

---
+++ Provinces/Scripts/SqlServer/data.sql
@@ -0,0 +1,68 @@
+USE [BasicInformation_GitHub]
+GO
+SET IDENTITY_INSERT [dbo].[Provinces] ON 
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (1, N'آذربایجان شرقی')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (2, N'آذربایجان غربی')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (3, N'اردبیل')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (4, N'اصفهان')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (5, N'البرز')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (6, N'ایلام')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (7, N'بوشهر')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (8, N'تهران')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (9, N'چهارمحال و بختیاری')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (10, N'خراسان جنوبی')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (11, N'خراسان رضوی')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (12, N'خراسان شمالی')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (13, N'	خوزستان')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (14, N'زنجان')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (15, N'سمنان')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (16, N'سیستان و بلوچستان')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (17, N'فارس')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (18, N'قزوین')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (19, N'قم')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (20, N'کردستان')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (21, N'کرمان')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (22, N'کرمانشاه')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (23, N'کهگیلویه و بویراحمد')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (24, N'گلستان')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (25, N'گیلان')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (26, N'	لرستان')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (27, N'مازندران	')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (28, N'مرکزی')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (29, N'هرمزگان')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (30, N'همدان')
+GO
+INSERT [dbo].[Provinces] ([ID], [Title]) VALUES (31, N'یزد')
+GO
+SET IDENTITY_INSERT [dbo].[Provinces] OFF
+GO
